a62826ec36e923fffed8994454b5ba01f970578d
Marchitan <marinela.marchitan@centric.eu>
2018-11-23 16:05:35 +0200

    Initial commit Mwise

---
+++ MWise-Azure/MWiseEndpoint/MWiseEndpoint/MWiseHttpClient.cs
@@ -0,0 +1,17 @@
+﻿namespace MWiseEndpoint
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    public class MWiseHttpClient
+    {
+        public HttpClient CreateClient()
+        {
+            var client = new HttpClient { Timeout = TimeSpan.FromSeconds(10), BaseAddress = new Uri("https://vomars2s-demo.m-point.eu/api/") };
+
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+            return client;
+        }
+    }
+}
